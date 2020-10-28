0b01bad79e96131fcea4b38665ae642e539abbce
JavierLagares99 <6lvjavierlr@gmail.com>
2020-10-28 08:53:29 +0100

    dji administration v.13

--- dji_administration/__manifest__.py
+++ dji_administration/__manifest__.py
@@ -1,29 +1,22 @@
 {
-    'name': 'D & HR Administrator',
-    'summary': """Administration settings for Discafé and Huelva Regalos""",
-    'version': '12.0.1.0.0', 
-    'description': """Administration settings foR Discafé and Huelva Regalos""",
-    'author': 'DDL',
+    'name': 'dji_administration',
+    'summary': """Administration settings for DJI""",
+    'version': '13.0.1.0.0',
+    'description': """Administration settings for DJI""",
+    'author': 'Javier Lagares',
     'company': 'Xtendoo',
-    'website': 'http://www.xtendoo.com',
+    'website': 'http://xtendoo.es',
     'category': 'Admin Tools',
     'depends': [
         'base',
-        'sale_margin',
-        'account_invoice_margin',
         'sale',
-        'account'
-
+        'product',
     ],
     'license': 'AGPL-3',
     'data': [
-        'views/view_users_form_create_invoice.xml',
-        'views/account_payment.xml',
         'views/sale_order_views.xml',
-        'views/sale_order_margin_view.xml',
-        'views/account_invoice_views.xml',
-        'views/account_invoice_margin_view.xml',
-        'views/product_template_view.xml',
+        'security/security_group.xml',
+        'views/product_views.xml'
     ],
     'installable': True,
     'auto_install': True,
--- dji_administration/models/product_template.py
+++ dji_administration/models/product_template.py
@@ -1,20 +1,8 @@
-from odoo import api, fields, models
+# -- coding: utf-8 --
 
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+from odoo import api, models, fields
 
-    is_admin = fields.Boolean(
-        comodel_name='product.template',
-        compute='_is_admin',
-        string="isAdmin",
-        default=lambda self: self._get_default_admin()
-    )
 
-    @api.one
-    def _is_admin(self):
-        self.is_admin = self.env.user.administration
-        return
-
-    @api.model
-    def _get_default_admin(self):
-        return self.env.user.administration
+class Product(models.Model):
+    _inherit = ['product.template', 'administrator.mixin.rule']
+    _name = 'product.template'

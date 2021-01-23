ae450d6832d47889c1ccf11cf3d72bae3f69d884
Manuel Calero <manuelcalerosolis@gmail.com>
2021-01-23 13:37:38 +0100

    city column

--- dji_administration/models/sale_order.py
+++ dji_administration/models/sale_order.py
@@ -6,3 +6,8 @@ from odoo import api, models, fields
 class SaleOrder(models.Model):
     _inherit = ['sale.order', 'administrator.mixin.rule']
     _name = 'sale.order'
+
+    city = fields.Char(
+        related='partner_id.city',
+        store=True,
+    )

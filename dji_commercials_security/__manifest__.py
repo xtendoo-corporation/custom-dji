8ca534d41c1863106e3bd184990bb8a71153ddbe
dani <dani.xtendoo@gmail.com>
2021-04-07 11:34:43 +0200

    security rules para comerciales en modulo aparte

---
+++ dji_commercials_security/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'dji_commercials_security',
+    'summary': """Restricciones de seguridad para los comerciales del grupo DJI""",
+    'version': '13.0.1.0.0',
+    'description': """Restricciones de seguridad para los comerciales del grupo DJI""",
+    'author': 'Dani Domínguez',
+    'company': 'Xtendoo',
+    'website': 'http://xtendoo.es',
+    'category': 'Admin Tools',
+    'depends': [
+        'dji_administration',
+    ],
+    'license': 'AGPL-3',
+    'data': [
+        'security/security_group.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}

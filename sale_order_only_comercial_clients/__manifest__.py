9945fae88f9fe0ff2b87f8f2a0e72dfd4c32ba45
dani <dani.xtendoo@gmail.com>
2021-04-12 12:44:58 +0200

    filtrado de clientes en ventas para comerciales

---
+++ sale_order_only_comercial_clients/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+###################################################################################
+#
+#
+###################################################################################
+
+{
+    "name": "Sale order only comercial clients",
+    "summary": """Sale order only comercial clients""",
+    "version": "13.0.1.0.0",
+    "description": """Sale order only comercial clients""",
+    "author": "Dani Domínguez",
+    "company": "Xtendoo",
+    "website": "https://xtendoo.es/",
+    "category": "Extra Tools",
+    "depends": [
+                "sale",
+                "dji_administration"
+                ],
+    "license": "AGPL-3",
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+}

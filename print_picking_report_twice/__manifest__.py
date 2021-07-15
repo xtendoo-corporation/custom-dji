cb910a91e423654171631878975e7c5e35918542
dani <dani.xtendoo@gmail.com>
2021-07-15 10:43:50 +0200

    añadido print_picking_report_twice

---
+++ print_picking_report_twice/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Print picking report twice",
+    "summary": """Permite la impresión de 2 copias de albarán""",
+    "version": "13.0.1.0.0",
+    "description": """Permite la impresión de 2 copias de albarán""",
+    "author": "Daniel Dominguez",
+    "company": "Xtendoo",
+    "website": "http://xtendoo.es",
+    "category": "Extra Tools",
+    "license": "AGPL-3",
+    "depends": [
+        "document_format",
+    ],
+    "data": [
+        "views/delivery/delivery_document.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}

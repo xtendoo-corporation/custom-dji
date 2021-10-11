c8b7dc6423d7ada9d6c9c775a08a024856972119
dani <dani.xtendoo@gmail.com>
2021-10-11 12:57:55 +0200

    Añadido recommended_products_sylonuba

---
+++ recommended_product_sylonuba/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Reoommended Product For Sylonuba',
+    'summary': """Reoommended Product For Sylonuba""",
+    'version': '13.0.1.0.0',
+    'description': """Reoommended Product For Sylonuba""",
+    'author': 'Dani Domínguez',
+    'company': 'Xtendoo',
+    'website': 'http://xtendoo.es',
+    'category': 'Admin Tools',
+    'depends': [
+        'sale',
+    ],
+    'license': 'AGPL-3',
+    'data': [
+        "wizards/sale_order_recommendation_view.xml",
+        "views/res_config_settings_views.xml",
+        "views/sale_order_view.xml",
+    ],
+    'installable': True,
+    'auto_install': True,
+}
+

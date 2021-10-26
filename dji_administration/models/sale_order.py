f4bb009fdf3607e66fcf67ed0259ee35b54a1f2e
dani <dani.xtendoo@gmail.com>
2021-10-26 10:08:57 +0200

    [ADD]Restriccion a la hora de confirmar un produto con precio 0 en alguna linea

--- dji_administration/models/sale_order.py
+++ dji_administration/models/sale_order.py
@@ -1,8 +1,17 @@
 # -- coding: utf-8 --
 
-from odoo import api, models, fields
+from odoo import api, models, fields,_
+from odoo.exceptions import ValidationError
 
 
 class SaleOrder(models.Model):
     _inherit = ['sale.order', 'administrator.mixin.rule']
     _name = 'sale.order'
+
+    def _action_confirm(self):
+        res = super(SaleOrder, self)._action_confirm()
+        for line in self.order_line:
+            if line.price_unit == 0.00:
+                raise ValidationError(
+                    _('No puede confirmar un pedido con precio 0.00 en alguna linea'))
+        return res

21a912e408d3ff06837da33b62dd1119b6efebfa
dani <dani.xtendoo@gmail.com>
2021-10-27 08:42:37 +0200

    [ADD]Añadido sale_order_only_comercial_clients para evitar ventas a 0 y quitado de dji_administration

--- dji_administration/models/sale_order.py
+++ dji_administration/models/sale_order.py
@@ -8,10 +8,3 @@ class SaleOrder(models.Model):
     _inherit = ['sale.order', 'administrator.mixin.rule']
     _name = 'sale.order'
 
-    def _action_confirm(self):
-        res = super(SaleOrder, self)._action_confirm()
-        for line in self.order_line:
-            if line.price_unit == 0.00:
-                raise ValidationError(
-                    _('No puede confirmar un pedido con precio 0.00 en alguna linea'))
-        return res
---
+++ sale_order_no_sale_without_price/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Sale Order Not sale without price',
+    'summary': """Sale Order Not sale without price""",
+    'version': '13.0.1.0.0',
+    'description': """Sale Order Not sale without price""",
+    'author': 'Dani Domínguez',
+    'company': 'Xtendoo',
+    'website': 'http://xtendoo.es',
+    'category': 'Admin Tools',
+    'depends': [
+        'sale',
+    ],
+    'license': 'AGPL-3',
+    'data': [
+
+    ],
+    'installable': True,
+    'auto_install': True,
+}
+

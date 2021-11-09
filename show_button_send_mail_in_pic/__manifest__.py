a5be1a7451775ebefab69683993c7db435bf71e7
dani <dani.xtendoo@gmail.com>
2021-11-09 08:09:50 +0100

    [ADD]Añadido show_button_send_mail_in_pic

---
+++ show_button_send_mail_in_pic/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Show button send mail in pic",
+    "summary": """Show button send mail in pic""",
+    "version": "13.0.1.0.0",
+    "description": """Show button send mail in pic""",
+    "author": "Daniel Dominguez",
+    "company": "Xtendoo",
+    "website": "http://xtendoo.es",
+    "category": "Extra Tools",
+    "license": "AGPL-3",
+    "depends": [
+        "stock_picking_send_by_mail",
+    ],
+    "data": [
+        "views/send_mail_view.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}

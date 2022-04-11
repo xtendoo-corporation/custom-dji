e2e5bed26ad5c805949f4800b8074e55f8859d3d
dani <dani.xtendoo@gmail.com>
2022-04-11 10:00:50 +0200

    [MOD] sale_order_no_Sale_without_price, quitada la restirccion, si no hay product_id (notas y secciones)

--- sale_order_no_sale_without_price/models/sale_order.py
+++ sale_order_no_sale_without_price/models/sale_order.py
@@ -11,14 +11,14 @@ class SaleOrder(models.Model):
     def _action_confirm(self):
         res = super(SaleOrder, self)._action_confirm()
         for line in self.order_line:
-            if line.price_unit == 0.00:
+            if line.price_unit == 0.00 and line.product_id:
                 raise ValidationError(
                     _('No puede confirmar un pedido con precio 0.00 en alguna linea'))
         return res
 
     def _write(self, value):
         for line in self.order_line:
-            if line.price_unit == 0.00:
+            if line.price_unit == 0.00 and line.product_id:
                 raise ValidationError(
                     _('No puede guardar un pedido con precio 0.00 en alguna linea'))
             print("*" * 80)

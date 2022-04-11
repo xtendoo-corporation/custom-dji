9d2255eee2b201bd7f224bdb09daf7e74e3f3072
dani <dani.xtendoo@gmail.com>
2022-04-11 09:55:53 +0200

    [ADD] sale_order_show_route_in_tree_view

---
+++ sale_order_show_route_in_tree_view/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Sale order show route in tree view',
+    'summary': """Sale order show route in tree view""",
+    'version': '13.0.1.0.0',
+    'description': """Sale order show route in tree view""",
+    'author': 'Dani Domínguez',
+    'company': 'Xtendoo',
+    'website': 'http://xtendoo.es',
+    'category': 'Admin Tools',
+    'depends': [
+        'sale_order_global_stock_route',
+    ],
+    'license': 'AGPL-3',
+    'data': [
+        'views/sale_order_view.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
+

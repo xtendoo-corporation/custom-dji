5f3b22e657e0b9e932e095a596d43ad6e33964ab
dani <dani.xtendoo@gmail.com>
2022-04-19 14:12:04 +0200

    [ADD] update_cost_fix

---
+++ update_cost_fix/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Update cost fix',
+    'summary': """Update cost fix""",
+    'version': '13.0.1.0.0',
+    'description': """Update cost fix""",
+    'author': 'Dani Domínguez',
+    'company': 'Xtendoo',
+    'website': 'http://xtendoo.es',
+    'category': 'Admin Tools',
+    'depends': [
+        'product',
+    ],
+    'license': 'AGPL-3',
+    'data': [
+
+    ],
+    'installable': True,
+    'auto_install': True,
+}
+

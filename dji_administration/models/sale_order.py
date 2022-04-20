9e64bbb84e11d6349b4005a029c59e138a9f38ec
Manuel Calero <manuelcalerosolis@gmail.com>
2022-04-20 19:50:08 +0200

    sync

--- dji_administration/models/sale_order.py
+++ dji_administration/models/sale_order.py
@@ -7,3 +7,14 @@ class SaleOrder(models.Model):
     _name = 'sale.order'
 
 
+    def _compute_picking_state_done(self):
+        for sale in self:
+            sale.is_picking_state_done = bool(sale.picking_ids.filtered(lambda p: p.state == 'done'))
+
+    is_picking_state_done = fields.Boolean(
+        compute="_compute_picking_state_done",
+        readonly=True,
+        multi="stock_reservation",
+        store=True,
+        string="Is Picking State Done",
+    )

a8e0e7949cf38ceb847e36404c7b6e17d0b227d3
dani.xtendoo <dani.xtendoo@gmail.com>
2023-07-17 11:20:56 +0200

    [ADD] sale_order_required_route

---
+++ sale_order_required_route/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Sale order redquired route',
+    'summary': """Sale order redquired route""",
+    'version': '13.0.1.0.0',
+    'description': """Sale order redquired route""",
+    'author': 'Dani Domínguez',
+    'company': 'Xtendoo',
+    'website': 'http://xtendoo.es',
+    'category': 'Admin Tools',
+    'depends': [
+        'base',
+        'sale_stock',
+    ],
+    'license': 'AGPL-3',
+    'data': [
+        'views/sale_order_view.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
+

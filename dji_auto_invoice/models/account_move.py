abc59010240cfdf5d5e3d79a976a3598e1571c0b
Dani <dani.xtendoo@gmail.com>
2023-08-28 14:15:47 +0200

    sync

--- dji_auto_invoice/models/account_move.py
+++ dji_auto_invoice/models/account_move.py
@@ -1,5 +1,4 @@
 from odoo import models, fields, api
-from odoo.tools.misc import get_lang
 
 
 class AccountMove(models.Model):
@@ -7,44 +6,23 @@ class AccountMove(models.Model):
 
     def post(self):
         res = super().post()
+        print("*"*120)
+        print("DEF POST")
         for invoice in self:
             if invoice.partner_id.email and invoice.move_type == 'receivable' and invoice.partner_id.auto_invoice:
+                print("ENTRA EN EL IF Y LLAMA A SEND MAIL")
                 invoice.send_email()
         return res
 
-
     def send_email(self):
-        template_id = (
-            self.env["mail.template"].search([("name", "=", "Invoice: Send by email")], limit=1).id
+        self.ensure_one()
+        template = self.env.ref('nombre_del_modulo.nombre_de_la_plantilla')
+        email_message = template.send_mail(self.id, force_send=True)
+        subject = email_message.subject or 'Factura enviada por correo electrónico'
+        message_body = f"Para: {self.partner_id.name}\n\n{email_message.body}"
+        self.message_post(
+            body=message_body,
+            message_type='comment',
+            subtype_id=self.env.ref('mail.mt_comment').id,
+            partner_ids=[(4, self.partner_id.id)]
         )
-        template = self.env["mail.template"].browse(template_id)
-        template.send_mail(self.id, force_send=True)
-
-        values = template.generate_email(self.id, ['subject', 'body_html', 'email_from', 'email_to', 'partner_to', 'email_cc', 'reply_to', 'scheduled_date'])
-        body_html = values['body_html']
-        doc = self.action_invoice_print()
-        Attachment = self.env['ir.attachment']
-
-        attachment_ids = values.pop('attachment_ids', [])
-        attachments = values.pop('attachments', [])
-
-        mail = self.env['mail.mail'].sudo().create(values)
-
-        for attachment in attachments:
-            attachment_data = {
-                'name': attachment[0],
-                'datas': attachment[1],
-                'type': 'binary',
-                'res_model': 'mail.message',
-                'res_id': mail.mail_message_id.id,
-            }
-            attachment_ids.append((4, Attachment.create(attachment_data).id))
-        if attachment_ids:
-            mail.write({'attachment_ids': attachment_ids})
-
-
-
-
-
-
-

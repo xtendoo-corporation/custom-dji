dbe6813360bd25a5e1e1c0d91189dc58c05aa6ab
Dani <dani.xtendoo@gmail.com>
2023-08-31 12:56:42 +0200

    sync

--- dji_auto_invoice/models/account_move.py
+++ dji_auto_invoice/models/account_move.py
@@ -6,17 +6,14 @@ class AccountMove(models.Model):
 
     def post(self):
         res = super().post()
-        print("*"*120)
-        print("DEF POST")
         for invoice in self:
             if invoice.partner_id.email and invoice.move_type == 'receivable' and invoice.partner_id.auto_invoice:
-                print("ENTRA EN EL IF Y LLAMA A SEND MAIL")
                 invoice.send_email()
         return res
 
     def send_email(self):
         self.ensure_one()
-        template = self.env.ref('nombre_del_modulo.nombre_de_la_plantilla')
+        template = self.env.ref('account.email_template_edi_invoice')
         email_message = template.send_mail(self.id, force_send=True)
         subject = email_message.subject or 'Factura enviada por correo electrónico'
         message_body = f"Para: {self.partner_id.name}\n\n{email_message.body}"

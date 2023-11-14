018ec7dd5e82ff483f1f4e7c5d49a3ea36343198
Dani <dani.xtendoo@gmail.com>
2023-11-14 10:30:09 +0100

    [ADD] purchase show date order always

---
+++ purchase_show_date_order_always/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Purchase show date order always",
+    "summary": """Mostrar la fecha de pedido siempre en vista tree de compras""",
+    "version": "14.0.1.0.0",
+    "description": """Mostrar la fecha de pedido siempre en vista tree de compras""",
+    "author": "DDL",
+    "company": "Xtendoo",
+    "website": "http://www.xtendoo.es",
+    "category": "Extra Tools",
+    "depends": ["base", "account", "sale", "web", "stock"],
+    "license": "AGPL-3",
+    "depends": ["stock"],
+    "data": [
+        "views/purchase_order.xml",
+    ],
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+}

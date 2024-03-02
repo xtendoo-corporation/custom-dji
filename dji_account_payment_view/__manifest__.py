00620353a229bf9999d6ffa051dc0b4cb68e5467
manuelcalerosolis <manuelcalerosolis@gmail.com>
2024-03-02 13:11:07 +0100

    account payment

---
+++ dji_account_payment_view/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2022 Xtendoo
+
+{
+    "name": "DJI Account Payment View",
+    "summary": "DJI Account Payment View",
+    "version": "15.0.1.0.0",
+    "depends": [
+        "base",
+        "account",
+    ],
+    "maintainers": [
+        "Manuel Calero",
+    ],
+    "author": "Xtendoo",
+    "license": "AGPL-3",
+    "data": [
+        "views/account_payment_view.xml",
+    ],
+    "application": True,
+    "installable": True,
+    "auto_install": True,
+}

d29aa1ca65906fb69db5ba8a5178501cc1851ef9
dani <dani.xtendoo@gmail.com>
2024-05-21 09:46:10 +0200

    [ADD] dji_no_date_expired

---
+++ dji_no_date_expired/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2022 Xtendoo
+
+{
+    "name": "DJI No Expired Date",
+    "summary": """
+        DJI No Expired Date""",
+    "version": "15.0.1.0.0",
+    "depends": [
+        "product_expiry",
+    ],
+    "maintainers": [
+        "Daniel Domínguez",
+    ],
+    "author": "Xtendoo",
+    "license": "AGPL-3",
+    "data": [
+    ],
+    "application": True,
+    "installable": True,
+    "auto_install": True,
+}

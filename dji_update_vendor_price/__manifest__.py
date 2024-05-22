e79008dc595ee3084a68d7fd9a2e2eaa06d52896
dani <dani.xtendoo@gmail.com>
2024-05-22 10:29:11 +0200

    [ADD] dji_update_vendor_price

---
+++ dji_update_vendor_price/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2022 Xtendoo
+
+{
+    "name": "DJI Update vendor price",
+    "summary": """
+        DJI Update vendor price""",
+    "version": "15.0.1.0.0",
+    "depends": [
+        "purchase",
+    ],
+    "maintainers": [
+        "Daniel Domínguez",
+    ],
+    "author": "Xtendoo",
+    "license": "AGPL-3",
+    "data": [
+    ],
+    "application": True,
+    "installable": True,
+    "auto_install": True,
+}
---
+++ dji_update_vendor_price/models/purchase_order.py
@@ -0,0 +1,51 @@
+from odoo import api, fields, models
+from odoo.exceptions import AccessError
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    def _add_supplier_to_product(self):
+        # Add the partner in the supplier list of the product if the supplier is not registered for
+        # this product. We limit to 10 the number of suppliers for a product to avoid the mess that
+        # could be caused for some generic products ("Miscellaneous").
+        for line in self.order_line:
+            # Do not add a contact as a supplier
+            partner = self.partner_id if not self.partner_id.parent_id else self.partner_id.parent_id
+            already_seller = (partner | self.partner_id) & line.product_id.seller_ids.mapped('name')
+            if already_seller:
+                currency = partner.property_purchase_currency_id or self.env.company.currency_id
+                price = self.currency_id._convert(line.price_unit, currency, line.company_id,
+                                                  line.date_order or fields.Date.today(), round=False)
+                supplier_info = line.product_id.seller_ids.filtered(lambda x: x.name == already_seller)
+                if supplier_info:
+                    if price != supplier_info.price:
+                        supplier_info.price = price
+            if line.product_id and not already_seller and len(line.product_id.seller_ids) <= 10:
+                # Convert the price in the right currency.
+                currency = partner.property_purchase_currency_id or self.env.company.currency_id
+                price = self.currency_id._convert(line.price_unit, currency, line.company_id,
+                                                  line.date_order or fields.Date.today(), round=False)
+                # Compute the price for the template's UoM, because the supplier's UoM is related to that UoM.
+                if line.product_id.product_tmpl_id.uom_po_id != line.product_uom:
+                    default_uom = line.product_id.product_tmpl_id.uom_po_id
+                    price = line.product_uom._compute_price(price, default_uom)
+
+                supplierinfo = self._prepare_supplier_info(partner, line, price, currency)
+                # In case the order partner is a contact address, a new supplierinfo is created on
+                # the parent company. In this case, we keep the product name and code.
+                seller = line.product_id._select_seller(
+                    partner_id=line.partner_id,
+                    quantity=line.product_qty,
+                    date=line.order_id.date_order and line.order_id.date_order.date(),
+                    uom_id=line.product_uom)
+                if seller:
+                    supplierinfo['product_name'] = seller.product_name
+                    supplierinfo['product_code'] = seller.product_code
+                vals = {
+                    'seller_ids': [(0, 0, supplierinfo)],
+                }
+                try:
+                    line.product_id.write(vals)
+                except AccessError:  # no write access rights -> just ignore
+                    break

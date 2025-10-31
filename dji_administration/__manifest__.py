de5365e50ddaf2e2ac3a05a6f241e39d69a3078c
dani <dani.xtendoo@gmail.com>
2025-10-31 11:14:16 +0100

    sync

--- dji_administration/__manifest__.py
+++ dji_administration/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Custom DJI Administration',
     'summary': """Administration settings for DJI""",
-    'version': '14.0.1.0.0',
+    'version': '15.0.1.0.0',
     'description': """Administration settings for DJI""",
     'author': 'Dani Domínguez',
     'company': 'Xtendoo',

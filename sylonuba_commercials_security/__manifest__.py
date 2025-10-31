2a183d9a12b68f70b1b159e517bbc4bd1b431983
dani <dani.xtendoo@gmail.com>
2025-10-31 09:23:32 +0100

    sync

---
+++ sylonuba_commercials_security/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'sylonuba_commercials_security',
+    'summary': """Restricciones de seguridad para los comerciales del grupo sylonuba""",
+    'version': '15.0.1.0.0',
+    'description': """Restricciones de seguridad para los comerciales del grupo sylonuba""",
+    'author': 'Dani Domínguez',
+    'company': 'Xtendoo',
+    'website': 'http://xtendoo.es',
+    'category': 'Admin Tools',
+    'depends': [
+        'dji_administration',
+    ],
+    'license': 'AGPL-3',
+    'data': [
+        'security/security_group.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}

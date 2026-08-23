e0a002e72260a7466923d4ef7fe218d5fdadd85e
manuelcalerosolis <manuelcalerosolis@gmail.com>
2026-08-23 19:41:18 +0200

    [MIG] sale_order_show_route_in_tree_view: Migration to 16.0

--- sale_order_show_route_in_tree_view/__manifest__.py
+++ sale_order_show_route_in_tree_view/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Sale order show route in tree view',
     'summary': """Sale order show route in tree view""",
-    'version': '16.0.1.0.0',
+    'version': "16.0.1.0.0",
     'description': """Sale order show route in tree view""",
     'author': 'Dani Domínguez',
     'company': 'Xtendoo',

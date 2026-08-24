eb80aac58b95611dd4d3be5c1424969e3fcb4f5f
manuelcalerosolis <manuelcalerosolis@gmail.com>
2026-08-24 07:04:13 +0200

    [MIG] bi_sale_order_merge: Migration to 17.0

--- bi_sale_order_merge/__manifest__.py
+++ bi_sale_order_merge/__manifest__.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 # Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
 {
     'name': 'Merge Sales Orders in Odoo',
-    'version': '17.0.1.4.6',
+    'version': "17.0.1.0.0",
     'category': 'Sales',
     'summary': 'Merge sale order merge sales order merge purchase order merge multiple sales order merge multiple purchase order merge mass sales order merge mass purchase order merger sale merger purchase merge Delivery order merge picking merge SO merge PO',
     'description': """
--- dji_update_vendor_price/models/purchase_order.py
+++ dji_update_vendor_price/models/purchase_order.py
@@ -7,23 +7,14 @@ class PurchaseOrder(models.Model):
 
     def _add_supplier_to_product(self):
         super()._add_supplier_to_product()
-        print("_add_supplier_to_product-------------------------------------------------")
         for line in self.order_line:
             # Do not add a contact as a supplier
-            print("line", line)
-            print("line.product_id", line.product_id)
-            print("line.product_id.name", line.product_id.name)
             partner = self.partner_id if not self.partner_id.parent_id else self.partner_id.parent_id
-            print("partner", partner)
-            already_seller = (partner | self.partner_id) & line.product_id.seller_ids.mapped('name')
-            print("already_seller", already_seller)
+            already_seller = (partner | self.partner_id) & line.product_id.seller_ids.mapped('partner_id')
             if already_seller:
                 currency = partner.property_purchase_currency_id or self.env.company.currency_id
                 price = self.currency_id._convert(
                     line.price_unit, currency, line.company_id, line.date_order or fields.Date.today(), round=False)
-                print("price", price)
-                supplier_info = line.product_id.seller_ids.filtered(lambda x: x.name == already_seller)
-                print("supplier_info", supplier_info)
+                supplier_info = line.product_id.seller_ids.filtered(lambda x: x.partner_id == already_seller)
                 if supplier_info and supplier_info.price != price:
-                    print("supplier_info.price", supplier_info.price)
                     supplier_info.price = price

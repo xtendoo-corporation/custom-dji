3f633ccf69f83856970af2a75d7abbafc3309c21
manuelcalerosolis <manuelcalerosolis@gmail.com>
2026-09-08 10:28:47 +0200

    sale_order_show_partner_city: activar vista de lista (estaba comentada, el campo Ciudad nunca se cargaba en v19)

--- sale_order_show_partner_city/__manifest__.py
+++ sale_order_show_partner_city/__manifest__.py
@@ -13,7 +13,7 @@
     ],
     "license": "AGPL-3",
     "data": [
-        #"views/sale_order_view.xml",
+        "views/sale_order_view.xml",
     ],
     "installable": True,
     "auto_install": False,
